104c67117c3c181fd3016f060fe1d39d69843721
ExpertSpace <02mago95@gmail.com>
2021-11-18 19:54:07 +0300

    Added file

---
+++ app/src/main/java/com/example/myapplication/rasl.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class rasl extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rasul_xml);
+    }
+
+
+}
